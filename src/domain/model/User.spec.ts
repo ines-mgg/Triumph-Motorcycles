a7d3d183510531514333d82880497490267249e9
ines-mgg <i.maganga@yahoo.com>
2024-10-03 19:57:56 +0200

    test: 💍 model
    added new test for user and new HTTP model

---
+++ src/domain/model/User.spec.ts
@@ -0,0 +1,22 @@
+import { expect, test, describe } from 'vitest';
+import { User } from './index';
+
+const validUser: User = {
+  id: 'validUser',
+  firstname: 'valid',
+  lastname: 'user',
+  email: 'validuser@test.com',
+  password: 'validUser',
+  role: 'ADMIN',
+};
+
+describe('testing User interface', () => {
+  describe('for validUser', () => {
+    test('user is defined', () => {
+      expect(validUser).toBeDefined();
+    });
+    test('user has right role', () => {
+      expect(['ADMIN', 'USER']).toContain(validUser.role);
+    });
+  });
+});
--- src/domain/model/User.ts
+++ src/domain/model/User.ts
@@ -1,5 +1,10 @@
+type Role = 'ADMIN' | 'USER';
+
 export interface User {
   id: string; // UUID
   firstname: string;
   lastname: string;
+  email: string;
+  password: string;
+  role: Role;
 }
