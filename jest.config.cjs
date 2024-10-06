f2f636743f5ccc03a055d11b9795741dc4538365
ines-mgg <i.maganga@yahoo.com>
2024-10-07 01:00:02 +0200

    test: 💍 jest.config.cjs
    update config to ignore cypress file

--- jest.config.cjs
+++ jest.config.cjs
@@ -8,4 +8,8 @@ module.exports = {
   moduleNameMapper: {
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
   },
+  testPathIgnorePatterns: [
+    '<rootDir>/cypress/',
+    '<rootDir>/.*\\.cy\\.[jt]sx?$',
+  ],
 };
