d16f74c1296bcc535394d66a12ac3c75b23d3a5c
ines-mgg <72012584+ines-mgg@users.noreply.github.com>
2025-02-02 17:48:02 +0100

    Install NextJS 15 (#15)
    ## What type of PR is this? (check all applicable)
    
    - [ ] :pizza: Feature
    - [ ] :bug: Bug(s) fix
    - [ ] :pencil: Wiki update
    - [ ] :lipstick: Style
    - [x] :fire: Performance improvements
    - [x] :white_check_mark: Tests (unit and/or E2E)
    - [ ] :repeat: CI/CD
    - [ ] :fast_forward: Revert
    
    ## Description
    
    ## Related Tickets & Documents
    
    - Related Issue #
    - Closes #
    
    ## UI accessibility checklist
    
    _If your PR includes UI changes, please utilize this checklist:_
    
    - [ ] Semantic HTML implemented?
    - [ ] Keyboard operability supported?
    - [ ] Checked with [axe DevTools](https://www.deque.com/axe/) and
    addressed `Critical` and `Serious` issues?
    - [ ] Color contrast tested with
    [webAIM](https://webaim.org/resources/contrastchecker/) ?
    
    ## Added/updated tests?
    
    - [x] :+1: Yes
    - [ ] :no*good: No, and this is why: \_please replace this line with
    details on why tests
          have not been included*
    - [ ] :raising_hand: I need help with writing tests
    
    ## [optional] Are there any post deployment tasks we need to perform?

--- src/infrastructure/frontend/cypress.config.ts
+++ src/infrastructure/frontend/cypress.config.ts
@@ -3,12 +3,8 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000/',
-  },
-
-  component: {
-    devServer: {
-      framework: 'next',
-      bundler: 'webpack',
-    },
+    // setupNodeEvents(on, config) {
+    //   // implement node event listeners here
+    // },
   },
 });
